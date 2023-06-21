5fb81a4b164b9afbc2bb41e344f27552ebeed2d6
onkar <onkarbjagtap2001@gmail.com>
2023-06-21 17:50:29 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { FormsModule } from '@angular/forms';
+
+
+import { AppComponent } from './app.component';
+import { FormComponentComponent } from './form-component/form-component.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FormComponentComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
